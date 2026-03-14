a8abad675690713317089d37be10c2e923780536
DingZiHeng <631573805@qq.com>
2026-03-14 14:56:20 +0800

    add the base class for the control layer

--- store/src/main/java/com/cy/store/controller/UserController.java
+++ store/src/main/java/com/cy/store/controller/UserController.java
@@ -14,10 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 // @Controller
 @RestController // @Controller + @ResponseBody
 @RequestMapping("users")
-public class UserController {
+public class UserController extends BaseController {
     @Autowired
     private IUserService userService;
 
+    @RequestMapping("reg")
+    // @ResponseBody 表示此方法的响应结果以json格式进行数据的响应给到前端
+    public JsonResult<Void> reg(User user){
+        userService.reg(user);
+        return new JsonResult<>(OK);
+    }
+
+    /*
     @RequestMapping("reg")
     // @ResponseBody 表示此方法的响应结果以json格式进行数据的响应给到前端
     public JsonResult<Void> reg(User user){
@@ -37,4 +45,5 @@ public class UserController {
 
         return result;
     }
+    */
 }
